7fc7d14ae59847235b55ae0ab7637c93a3eb4d9a
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-16 23:08:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
   answer = []
   holiday_hash.each do |season, data|
   data.each do |holiday, value|
-    value.each { |supply| answer.push(holiday) if supply == "BBQ") }
+    value.each { |supply| answer.push(holiday) if supply == "BBQ" }
     end
   end
   answer
